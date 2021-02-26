8a4f56de942cdc36ffcba95eaa377039809c6d2c
LalithK90 <asakahatapitiya@gmail.com>
2021-02-26 10:21:41 +0530

    Dish related

---
+++ src/main/java/lk/mongo_with_spring/asset/dish/Dish.java
@@ -0,0 +1,32 @@
+package lk.mongo_with_spring.asset.dish;
+
+import lk.mongo_with_spring.asset.dish.enums.Format;
+import lk.mongo_with_spring.asset.ingredient.Ingredients;
+import lk.mongo_with_spring.asset.restaurant_account.RestaurantAccount;
+import lk.mongo_with_spring.asset.review.Review;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Document
+@Data
+public class Dish {
+  @Id
+  private int id;
+
+  private String name,description;
+
+  private BigDecimal price;
+//todo-> need to more clarification about this
+  private Format format;
+
+  private RestaurantAccount restaurantAccount;
+
+  private List<Review> reviews;
+
+  private List< Ingredients > ingredients;
+
+}
---
+++ src/main/java/lk/mongo_with_spring/asset/dish/enums/Format.java
@@ -0,0 +1,10 @@
+package lk.mongo_with_spring.asset.dish.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Format {
+//todo -> not clear what was the format
+  CHILD,ADULTS,ELDERS
+
+}
