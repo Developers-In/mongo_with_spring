8e2ecb5648362b92efc2cb16eca5a36a4ead10dd
LalithK90 <asakahatapitiya@gmail.com>
2021-03-01 19:23:40 +0530

    Allergy category and categories were added

--- src/main/java/lk/mongo_with_spring/asset/allergy/Allergy.java
+++ src/main/java/lk/mongo_with_spring/asset/allergy/Allergy.java
@@ -1,13 +1,9 @@
 package lk.mongo_with_spring.asset.allergy;
 
 import lk.mongo_with_spring.asset.allergy.enums.AllergyCategory;
-import lk.mongo_with_spring.asset.customer.Customer;
-import lk.mongo_with_spring.asset.ingredient.Ingredient;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.List;
 
 @Data
 @Document
@@ -19,7 +15,4 @@ public class Allergy {
 
   private AllergyCategory allergyCategory;
 
-  private List< Customer > customers;
-
-  private List< Ingredient > ingredients;
 }
